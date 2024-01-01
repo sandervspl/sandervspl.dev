ecb8fb8db7223b7ac7203c20dce3c4c726f54b4c
Sander Vispoel <sandervispoel@hotmail.com>
2024-01-01 18:33:14 +0100

    Update md template

--- scripts/markdown-to-html.ts
+++ scripts/markdown-to-html.ts
@@ -19,12 +19,29 @@ type Meta = {
 const html = String.raw;
 
 function template(content: string, meta: Meta) {
-  return html`
-<!DOCTYPE html>
+  return html`<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="/dist/styles.css" rel="stylesheet" />
     ${meta.title ? `<title>${meta.title}</title>` : ''}
+    <meta
+      name="description"
+      content="Sander Vispoel is a fullstack developer from The Netherlands."
+    />
+    <link
+      rel="icon"
+      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐠</text></svg>"
+    />
+    <script>
+      window.va =
+        window.va ||
+        function () {
+          (window.vaq = window.vaq || []).push(arguments);
+        };
+    </script>
+    <script defer src="/_vercel/insights/script.js"></script>
   </head>
   <body class="prose">
     ${content}
