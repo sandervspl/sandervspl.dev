29599c780f8c60c5f55902ca4016188e3a4ac3fe
Sander Vispoel <sandervispoel@hotmail.com>
2025-05-30 19:19:21 +0200

    Fix icons

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -19,14 +19,6 @@ export function Head({
         rel="icon"
         href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐠</text></svg>"
       />
-      {/* <script>
-				{`window.va =
-            window.va ||
-            function () {
-              (window.vaq = window.vaq || []).push(arguments);
-            };
-          `}
-			</script> */}
       <script
         defer
         data-domain="sandervspl.dev"
---
+++ src/svg-jsx.d.ts
@@ -0,0 +1,5 @@
+declare module 'jsx:*svg' {
+  import type * as React from 'react';
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
